02957447c4989a3b6026da96990273a6e95fd797
Constantin Hager <constantin.hager@outlook.de>
2020-01-04 18:13:36 +0100

    fix

--- Terraform/SonarQube/modules/azure_appservice/main.tf
+++ Terraform/SonarQube/modules/azure_appservice/main.tf
@@ -7,4 +7,14 @@ resource "azurerm_app_service" "appservice" {
   site_config {
     linux_fx_version = "DOCKER|${var.ACRLoginServer}/sonarqube:v1"
   }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = false
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://${var.ACRLoginServer}"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = "${var.ACRUserName}"
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = "${var.ACRPassword}"
+    "sonar.jdbc.username"                 = "jdbc:sqlserver://${var.SQLServerFQDN};databaseName=${var.DBName}"
+    "sonar.jdbc.password"                 = "${var.AdminUserName}"
+    "sonar.jdbc.url"                      = "${var.AdminPassword}"
+  }
 }
