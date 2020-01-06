c8f71f804e643a1126a16c9b577208f4ac8d7d92
Constantin Hager <constantin.hager@outlook.de>
2020-01-06 12:10:28 +0100

    update service plan

--- Terraform/SonarQube/modules/azure_appserviceplan/main.tf
+++ Terraform/SonarQube/modules/azure_appserviceplan/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   reserved            = true
 
   sku {
-    tier = "Standard"
-    size = "F1"
+    tier = "PremiumV2"
+    size = "P1v2"
   }
 }
