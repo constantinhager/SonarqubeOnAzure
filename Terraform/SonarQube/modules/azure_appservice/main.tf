a9b32d8cd623834242f67227b4e59cd3ff437b93
Constantin Hager <constantin.hager@outlook.de>
2020-01-06 10:37:00 +0100

    fix

--- Terraform/SonarQube/modules/azure_appservice/main.tf
+++ Terraform/SonarQube/modules/azure_appservice/main.tf
@@ -20,24 +20,24 @@
 } */
 
 resource "azurerm_template_deployment" "appservice" {
-  name = "appservice-ARM"
+  name                = "appservice-ARM"
   resource_group_name = "${var.resourcegroupName}"
 
   template_body = "${file(appservice.json)}"
 
   deployment_mode = "Incremental"
 
-  parameters {
-    "siteName" = "${var.AppServiceName}"
-    "location" = "${var.location}"
-    "servicePlanName" = "${var.AppServicePlanId}"
-    "sqlServerName" = "${var.SQLServerFQDN}"
-    "databaseName" = "${var.DBName}"
+  parameters = {
+    "siteName"                       = "${var.AppServiceName}"
+    "location"                       = "${var.location}"
+    "servicePlanName"                = "${var.AppServicePlanId}"
+    "sqlServerName"                  = "${var.SQLServerFQDN}"
+    "databaseName"                   = "${var.DBName}"
     "sqlServerAdministratorUsername" = "${var.AdminUserName}"
     "sqlServerAdministratorPassword" = "${var.AdminPassword}"
-    "dockerServerURL" = "${var.ACRLoginServer}"
-    "ACRUserName" = "${var.ACRUserName}"
-    "ACRPassword" = "${var.ACRPassword}"
+    "dockerServerURL"                = "${var.ACRLoginServer}"
+    "ACRUserName"                    = "${var.ACRUserName}"
+    "ACRPassword"                    = "${var.ACRPassword}"
   }
 }
 
